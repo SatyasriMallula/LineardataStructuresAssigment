7583d8f2607050aca9a48ca1b3756d6ebb59d541
SatyasriMallula <108943119+SatyasriMallula@users.noreply.github.com>
2023-01-16 18:51:58 +0530

    Add files via upload

---
+++ LinearDataStructuresAssignment/Assignment.js
@@ -0,0 +1,359 @@
+// 1 Delete the elements in an linked list whose sum is equal to zero
+class LinkedListNode {
+    constructor(val) {
+        this.val = val
+        this.next = null
+    }
+}
+
+// Function to create Node
+function getNode(data) {
+    let temp = new LinkedListNode(data)
+    temp.next = null
+    return temp
+}
+function printList(head) {
+    while (head.next) {
+        document.write(head.val, ' -> ')
+        head = head.next
+    }
+    document.write(head.val, '')
+}
+function removeZeroSum(head, K) {
+    let root = new LinkedListNode(0)
+    root.next = head
+    let umap = new Map();
+    umap.set(0, root)
+    let sum = 0
+    while (head != null) {
+        sum += head.val
+        if (umap.has(sum - K) == true) {
+            let prev = umap.get(sum - K)
+            let start = prev
+            let aux = sum
+            sum = sum - K
+            while (prev != head) {
+                prev = prev.next
+                aux += prev.val
+                if (prev != head)
+                    umap.delete(aux)
+            }
+            start.next = head.next
+        }
+        else
+            umap.set(sum, head)
+        head = head.next
+    }
+    return root.next
+}
+let head = getNode(1)
+head.next = getNode(2)
+head.next.next = getNode(-3)
+head.next.next.next = getNode(3)
+head.next.next.next.next = getNode(1)
+let K = 5
+head = removeZeroSum(head, K)
+if (head != null)
+    printList(head)
+
+// 2 Javascript program to merge a linked list into another at alternate positions a nexted list node
+class ListNode {
+    constructor() {
+        this.data = 0;
+        this.next = null;
+    }
+};
+
+function push(head_ref, new_data) {
+    var new_node = new ListNode();
+    new_node.data = new_data;
+    new_node.next = (head_ref);
+    (head_ref) = new_node;
+    return head_ref;
+
+}
+
+function printList(head) {
+    var temp = head;
+    while (temp != null) {
+        console.log(temp.data + " ");
+        temp = temp.next;
+    }
+    console.log("<br>");
+}
+
+function merge(p, q) {
+    var p_curr = p, q_curr = q;
+    var p_next, q_next;
+    while (p_curr != null && q_curr != null) {
+        p_next = p_curr.next;
+        q_next = q_curr.next;
+        q_curr.next = p_next;
+        p_curr.next = q_curr;
+        p_curr = p_next;
+        q_curr = q_next;
+    }
+    q = q_curr;
+    return q;
+}
+var p = null, q = null;
+p = push(p, 4);
+p = push(p, 5);
+p = push(p, 2);
+console.log("First Linked List:<br>");
+printList(p);
+q = push(q, 5);
+q = push(q, 3);
+q = push(q, 8);
+q = push(q, 4);
+q = push(q, 9);
+console.log("Second Linked List:<br>");
+printList(q);
+q = merge(p, q);
+console.log("Modified First Linked List:<br>");
+printList(p);
+console.log("Modified Second Linked List:<br>");
+printList(q);
+
+// 3.Merge a linked list into another linked list at alternate positions.
+class Node {
+    constructor() {
+        this.data = 0;
+        this.next = null;
+    }
+};
+function push(head_ref, new_data) {
+    var new_node = new Node();
+    new_node.data = new_data;
+    new_node.next = (head_ref);
+    (head_ref) = new_node;
+    return head_ref;
+}
+function printList(head) {
+    var temp = head;
+    while (temp != null) {
+        console.log(temp.data + " ");
+        temp = temp.next;
+    }
+    console.log("<br>");
+}
+
+function merge(p, q) {
+    var p_curr = p, q_curr = q;
+    var p_next, q_next;
+    while (p_curr != null && q_curr != null) {
+        p_next = p_curr.next;
+        q_next = q_curr.next;
+        q_curr.next = p_next;
+        p_curr.next = q_curr;
+        p_curr = p_next;
+        q_curr = q_next;
+    }
+    q = q_curr;
+    return q;
+}
+var p = null, q = null;
+p = push(p, 4);
+p = push(p, 5);
+p = push(p, 2);
+console.log("First Linked List:<br>");
+printList(p);
+q = push(q, 5);
+q = push(q, 3);
+q = push(q, 8);
+q = push(q, 4);
+q = push(q, 9);
+console.log("Second Linked List:<br>");
+printList(q);
+q = merge(p, q);
+console.log("Modified First Linked List:<br>");
+printList(p);
+console.log("Modified Second Linked List:<br>");
+printList(q);
+
+// 4 In an array, Count Pairs with given sum
+
+function getPairsCount(arr, n, sum) {
+    let count = 0;
+    for (let i = 0; i < n; i++)
+        for (let j = i + 1; j < n; j++)
+            if (arr[i] + arr[j] == sum)
+                count++;
+    return count;
+}
+let arr1 = [1, 5, 7, -1, 5];
+let n = arr.length;
+let sum = 6;
+console.log("Count of pairs is "
+    + getPairsCount(arr, n, sum));
+
+//5 Find duplicates in an array
+const a = [8, 3, 2, 8, 9, 3]
+
+function count_duplicate(a) {
+    let counts = {}
+    for (let i = 0; i < a.length; i++) {
+        if (counts[a[i]]) {
+            counts[a[i]] += 1
+        } else {
+            counts[a[i]] = 1
+        }
+    }
+    for (let prop in counts) {
+        if (counts[prop] >= 2) {
+            console.log(prop + " counted: " + counts[prop] + " times.")
+        }
+    }
+    console.log(counts)
+}
+
+count_duplicate(a)
+
+/* 6 max and min array value */
+let arrayList = [11, 12, 3, 41, 3, 2, 10];
+let maxNum = arrayList.reduce((prev, current) => {
+    return Math.max(prev, current)
+});
+let minNum = arrayList.reduce((prev, current) => {
+    return Math.min(prev, current)
+});
+console.log(maxNum);
+console.log(minNum);
+
+
+// 7 A JavaScript program to put all negative numbers before positive numbers
+function move(arr) {
+    arr.sort();
+}
+
+let arr = [1, -2, -9, 4, -5, 6, 7, 8, 9];
+move(arr);
+for (let e of arr)
+    document.write(e, " ");
+
+
+// 8 Reverse a string using a stack data structure
+var head1;
+class Node1 {
+    constructor(val) {
+        this.data = val;
+        this.next = null;
+    }
+}
+function reverse(head1, k) {
+    if (head1 == null)
+        return null;
+    var current = head1;
+    var next = null;
+    var prev = null;
+    var count = 0;
+    while (count < k && current != null) {
+        next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+        count++;
+    }
+    if (next != null)
+        head1.next = reverse(next, k);
+    return prev;
+}
+function push(new_data) {
+
+    let new_node = new Node1(new_data);
+    new_node.next = head1;
+    head1 = new_node;
+}
+function printList() {
+    temp = head1;
+    while (temp != null) {
+        console.log(temp.data + " ");
+        temp = temp.next;
+    }
+    console.log("<br/>");
+}
+push(6);
+push(5);
+push(4);
+push(3);
+push(2);
+push(1);
+console.log("Given Linked List");
+printList();
+head1 = reverse(head1, 3);
+console.log("Reversed list");
+printList();
+
+/* 9 Evaluate a postfix expression using stack*/
+function evaluatePostfix(exp) {
+    let stack = [];
+    for (let i = 0; i < exp.length; i++) {
+        let c = exp[i];
+        if (c == ' ') {
+            continue;
+        }
+        else if (c >= '0' && c <= '9') {
+            let n = 0;
+            while (c >= '0' && c <= '9') {
+                n = n * 10 + (c - '0');
+                i++;
+                c = exp[i];
+            }
+            i--;
+            stack.push(n);
+        }
+        else {
+            let val1 = stack.pop();
+            let val2 = stack.pop();
+            switch (c) {
+                case '+':
+                    stack.push(val2 + val1);
+                    break;
+                case '-':
+                    stack.push(val2 - val1);
+                    break;
+                case '/':
+                    stack.push(parseInt(val2 / val1, 10));
+                    break;
+                case '*':
+                    stack.push(val2 * val1);
+                    break;
+            }
+        }
+    }
+    return stack.pop();
+}
+let exp = "100 200 + 2 / 5 * 7 +";
+console.log(evaluatePostfix(exp));
+
+//10 Implement a queue using the stack data structure
+class Queue {
+    constructor() {
+        this.s1 = [];
+        this.s2 = [];
+    }
+    enQueue(x) {
+        while (this.s1.length != 0) {
+            this.s2.push(this.s1.pop());
+        }
+        this.s1.push(x);
+        while (this.s2.length != 0) {
+            this.s1.push(this.s2.pop());
+        }
+    }
+    deQueue() {
+        if (this.s1.length == 0) {
+            console.log("Q is Empty");
+        }
+        let x = this.s1[this.s1.length - 1];
+        this.s1.pop();
+        return x;
+    }
+}
+let q1 = new Queue();
+q1.enQueue(1);
+q1.enQueue(2);
+q1.enQueue(3);
+console.log(q1.deQueue());
+console.log(q1.deQueue());
+console.log(q1.deQueue());
